cd73df1643b46306fc4a0063999ce948190a08fc
Darkhail <darkhail17@gmail.com>
2023-07-13 01:37:12 -0400

    Adding custom item properties

--- src/main/java/net/darkhail/mccourse/MCCourseMod.java
+++ src/main/java/net/darkhail/mccourse/MCCourseMod.java
@@ -4,6 +4,7 @@ import com.mojang.logging.LogUtils;
 import net.darkhail.mccourse.block.ModBlocks;
 import net.darkhail.mccourse.enchantment.ModEnchantments;
 import net.darkhail.mccourse.item.ModCreativeModeTabs;
+import net.darkhail.mccourse.item.ModItemProperties;
 import net.darkhail.mccourse.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
@@ -89,7 +90,10 @@ public class MCCourseMod
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event)
         {
+            event.enqueueWork(() -> {
+                ModItemProperties.addCustomItemProperties();
 
+            });
         }
     }
 }
