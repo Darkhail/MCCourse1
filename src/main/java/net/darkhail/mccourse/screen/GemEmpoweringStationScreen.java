49baeb8a149e68dc3bf744076d00cdeea4bcc8c1
Darkhail <darkhail17@gmail.com>
2023-09-17 16:41:27 -0400

    Energy input

--- src/main/java/net/darkhail/mccourse/screen/GemEmpoweringStationScreen.java
+++ src/main/java/net/darkhail/mccourse/screen/GemEmpoweringStationScreen.java
@@ -1,6 +1,8 @@
 package net.darkhail.mccourse.screen;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.darkhail.mccourse.MCCourseMod;
+import net.darkhail.mccourse.screen.renderer.EnergyDisplayTooltipArea;
+import net.darkhail.mccourse.util.MouseUtil;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
@@ -8,9 +10,12 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
+import java.util.Optional;
+
 public class GemEmpoweringStationScreen extends AbstractContainerScreen<GemEmpoweringStationMenu> {
     private static final ResourceLocation TEXTURE =
             new ResourceLocation(MCCourseMod.MOD_ID,"textures/gui/gem_empowering_station_gui.png");
+    private EnergyDisplayTooltipArea energyInfoArea;
 
     public GemEmpoweringStationScreen(GemEmpoweringStationMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
@@ -21,6 +26,29 @@ public class GemEmpoweringStationScreen extends AbstractContainerScreen<GemEmpow
         super.init();
         this.inventoryLabelY = 10000;
         this.titleLabelY = 10000;
+
+        assignEnergyInfoArea();
+    }
+
+    @Override
+    protected void renderLabels(GuiGraphics guiGraphics, int pMouseX, int pMouseY) {
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+
+        renderEnergyAreaTooltip(guiGraphics, pMouseX, pMouseY, x, y);
+    }
+
+    private void renderEnergyAreaTooltip(GuiGraphics guiGraphics, int pMouseX, int pMouseY, int x, int y) {
+        if(isMouseAboveArea(pMouseX, pMouseY, x, y, 156, 11, 8, 64)) {
+            guiGraphics.renderTooltip(this.font, energyInfoArea.getTooltips(),
+                    Optional.empty(), pMouseX - x, pMouseY - y);
+        }
+    }
+
+    private void assignEnergyInfoArea(){
+        energyInfoArea = new EnergyDisplayTooltipArea(((width - imageWidth) / 2) + 156,
+                ((height - imageHeight) / 2) + 11, menu.blockEntity.getEnergyStorage());
+
     }
 
     @Override
@@ -34,6 +62,8 @@ public class GemEmpoweringStationScreen extends AbstractContainerScreen<GemEmpow
         guiGraphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
 
         renderProgressArrow(guiGraphics, x, y);
+
+        energyInfoArea.render(guiGraphics);
     }
 
     private void renderProgressArrow(GuiGraphics guiGraphics, int x, int y) {
@@ -47,5 +77,9 @@ public class GemEmpoweringStationScreen extends AbstractContainerScreen<GemEmpow
         renderBackground(guiGraphics);
         super.render(guiGraphics, mouseX, mouseY, delta);
         renderTooltip(guiGraphics, mouseX, mouseY);
+    }
+
+    private boolean isMouseAboveArea(int pMouseX, int pMouseY, int x, int y, int offsetX, int offsetY, int width, int height) {
+        return MouseUtil.isMouseOver(pMouseX, pMouseY, x + offsetX, y + offsetY, width, height);
     }
 }
