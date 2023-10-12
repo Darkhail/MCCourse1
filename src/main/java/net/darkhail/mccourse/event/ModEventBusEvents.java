30572b8983f5f3373ad69f8d57227ff4a92ace7a
Darkhail <darkhail17@gmail.com>
2023-10-12 01:22:06 -0400

    Spawn the Rhinos in the wild!

--- src/main/java/net/darkhail/mccourse/event/ModEventBusEvents.java
+++ src/main/java/net/darkhail/mccourse/event/ModEventBusEvents.java
@@ -5,8 +5,12 @@ import net.darkhail.mccourse.entity.ModEntities;
 import net.darkhail.mccourse.entity.client.RhinoModel;
 import net.darkhail.mccourse.entity.custom.RhinoEntity;
 import net.darkhail.mccourse.entity.layers.ModModelLayers;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -20,6 +24,11 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.RHINO.get(), RhinoEntity.createAttributes().build());
+    }
+
+    public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
+        event.register(ModEntities.RHINO.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Animal::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 
 }
