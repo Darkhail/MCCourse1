82ff6e824e7c777fcb708f5d8f9283076302d500
Darkhail <darkhail17@gmail.com>
2023-10-22 15:31:46 -0400

    sus sand items

--- src/main/java/net/darkhail/mccourse/datagen/ModGlobalLootModifierProvider.java
+++ src/main/java/net/darkhail/mccourse/datagen/ModGlobalLootModifierProvider.java
@@ -3,6 +3,7 @@ package net.darkhail.mccourse.datagen;
 import net.darkhail.mccourse.MCCourseMod;
 import net.darkhail.mccourse.item.ModItems;
 import net.darkhail.mccourse.loot.AddItemModifier;
+import net.darkhail.mccourse.loot.AddSusSandItemModifier;
 import net.darkhail.mccourse.util.ModTags;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
@@ -29,6 +30,10 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
 
         add("metal_detector_from_jungle_temple", new AddItemModifier(new LootItemCondition[]{
                 new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build() },
+                ModItems.METAL_DETECTOR.get()));
+
+        add("metal_detector_from_suspicious_sand", new AddSusSandItemModifier(new LootItemCondition[]{
+                new LootTableIdCondition.Builder(new ResourceLocation("archaeology/desert_pyramid")).build() },
                 ModItems.METAL_DETECTOR.get()));
     }
 }
